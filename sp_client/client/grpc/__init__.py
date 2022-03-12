e922f3647b41372a7485cc8ddefdce08b626201f
Abinash Biswal <abinash.biswal@spinny.com>
2022-03-13 01:06:48 +0530

    added client lib for server2

--- sp_client/client/grpc/__init__.py
+++ sp_client/client/grpc/__init__.py
@@ -1,4 +1,7 @@
 from sp_client.client.grpc.protos.post_pb2_grpc import PostControllerStub
 from sp_client.client.grpc.protos.post_pb2 import Post, PostListRequest, PostRetrieveRequest
+from sp_client.client.grpc.protos.server2_pb2_grpc import GetControllerStub
+from sp_client.client.grpc.protos.server2_pb2 import GetListRequest
 
-__all__ = ["PostControllerStub", "Post", "PostListRequest", "PostRetrieveRequest"]
+__all__ = ["PostControllerStub", "Post", "PostListRequest", "PostRetrieveRequest",
+           "GetControllerStub", "GetListRequest"]
--- sp_client/client/lib/sp_client_lib.py
+++ sp_client/client/lib/sp_client_lib.py
@@ -1,11 +1,12 @@
 import grpc
 from sp_client.client.grpc import PostControllerStub, \
-    Post, PostRetrieveRequest, PostListRequest
+    Post, PostRetrieveRequest, PostListRequest, GetControllerStub, GetListRequest
 
 class Operation:
     def __init__(self):
         self.channel = grpc.insecure_channel('localhost:50051')
         self.stub = PostControllerStub(self.channel)
+        self.server2_stub = GetControllerStub(self.channel)
     
     def create_post(self, title=None, content=None):
         response = self.stub.Create(Post(title=title, content=content))
@@ -14,4 +15,8 @@ class Operation:
     def get_post(self, id=None):
         response = self.stub.Retrieve(PostRetrieveRequest(id=id))
         print("get resp: ", response)
+        return response
+    def get_from_server2(self, id=1):
+        response = self.server2_stub.List(GetListRequest(id=1))
+        print("get resp from server2: ", response)
         return response
