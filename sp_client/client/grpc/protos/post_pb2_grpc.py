357bb19836bb796dd0d0b12278ff414f330d09df
Abinash Biswal <abinash.biswal@spinny.com>
2022-03-12 21:06:02 +0530

    minor

--- sp_client/client/grpc/protos/post_pb2_grpc.py
+++ sp_client/client/grpc/protos/post_pb2_grpc.py
@@ -2,7 +2,7 @@
 import grpc
 
 from google.protobuf import empty_pb2 as google_dot_protobuf_dot_empty__pb2
-import post_pb2 as post__pb2
+from . import post_pb2 as post__pb2
 
 
 class PostControllerStub(object):
